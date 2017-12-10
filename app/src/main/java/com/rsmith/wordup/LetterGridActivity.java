a9110abb1fe56ca69123cb122326a9bf1923ba5f
rsmith8917 <rsmith8917@gmail.com>
2017-12-09 19:15:39 -0500

    Drag to create words.  Started implementing connection to oxford dictionary API

--- app/src/main/java/com/rsmith/wordup/LetterGridActivity.java
+++ app/src/main/java/com/rsmith/wordup/LetterGridActivity.java
@@ -1,7 +1,10 @@
 package com.rsmith.wordup;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,11 +15,17 @@ public class LetterGridActivity extends AppCompatActivity {
 
     private LetterGenerator letterGen;
     private TextView[] lettersTextView = new TextView[25];
+    private TextView word;
+    private int[] upperLeft = new int[2];
+    private int[] lowerRight = new int[2];
+    private int lastCol = -1;
+    private int lastRow = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_letter_grid);
+        word = findViewById(R.id.word);
         letterGen = new LetterGenerator();
         String letters = new String(letterGen.getLetters());
         lettersTextView[0] = this.findViewById(R.id.textView00);
@@ -45,18 +54,108 @@ public class LetterGridActivity extends AppCompatActivity {
         lettersTextView[23] = this.findViewById(R.id.textView43);
         lettersTextView[24] = this.findViewById(R.id.textView44);
 
+
         Button clickButton = (Button) findViewById(R.id.button);
         clickButton.setOnClickListener( new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 char[] letters = new char[25];
                 letters = letterGen.getLetters();
                 for (int i = 0; i < 25; i++){
-                    lettersTextView[i].setText(Character.toString(letters[i]));
+                    if(letters[i] == 'Q'){
+                        lettersTextView[i].setText("Qu");
+                    } else {
+                        lettersTextView[i].setText(Character.toString(letters[i]));
+                    }
                 }
             }
         });
+
+    }
+
+    @Override
+    public void onWindowFocusChanged (boolean hasFocus) {
+        lettersTextView[0].getLocationOnScreen(upperLeft);
+        lettersTextView[24].getLocationOnScreen(lowerRight);
+        lowerRight[0] = lowerRight[0] + lettersTextView[24].getWidth();
+        lowerRight[1] = lowerRight[1] + lettersTextView[24].getHeight() - upperLeft[1];
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        double x0 = event.getX() - upperLeft[0];
+        double y0 = event.getY() - upperLeft[1];
+        int col = 0;
+        int row = 0;
+        int index = 0;
+
+        try{
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    double colDouble = ((x0 / lowerRight[0]) * 5);
+                    double rowDouble = ((y0 / lowerRight[1]) * 5);
+                    int colRemainder = (int)((colDouble * 10) % 10);
+                    int rowRemainder = (int)((rowDouble * 10) % 10);
+                    col = (int) colDouble;
+                    row = (int) rowDouble;
+                    index = col + (row * 5);
+                    if (col != lastCol || row != lastRow) {
+                        if (2 < colRemainder && 8 > colRemainder && 2 < rowRemainder && 8 > rowRemainder) {
+                            String text = this.word.getText().toString() + lettersTextView[index].getText();
+                            this.word.setText(text.toUpperCase());
+                            for (int i = 0; i < 25; i++) {
+                                if (i == index){
+                                    lettersTextView[i].setTypeface(null, Typeface.BOLD);
+                                    lettersTextView[i].setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                                } else {
+                                    lettersTextView[i].setTypeface(null, Typeface.NORMAL);
+                                    lettersTextView[i].setBackgroundColor(0xFFFFFF);
+                                }
+                            }
+                            lastCol = col;
+                            lastRow = row;
+                        }
+                    }
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    colDouble = ((x0 / lowerRight[0]) * 5);
+                    rowDouble = ((y0 / lowerRight[1]) * 5);
+                    colRemainder = (int)((colDouble * 10) % 10);
+                    rowRemainder = (int)((rowDouble * 10) % 10);
+                    col = (int) colDouble;
+                    row = (int) rowDouble;
+                    index = col + (row * 5);
+                    if (col != lastCol || row != lastRow) {
+                        if (2 < colRemainder && 8 > colRemainder && 2 < rowRemainder && 8 > rowRemainder) {
+                            String text = this.word.getText().toString() + lettersTextView[index].getText();
+                            this.word.setText(text.toUpperCase());
+                            for (int i = 0; i < 25; i++) {
+                                if (i == index){
+                                    lettersTextView[i].setTypeface(null, Typeface.BOLD);
+                                    lettersTextView[i].setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                                } else {
+                                    lettersTextView[i].setTypeface(null, Typeface.NORMAL);
+                                    lettersTextView[i].setBackgroundColor(0xFFFFFF);
+                                }
+                            }
+                            lastCol = col;
+                            lastRow = row;
+                        }
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    new OxfordDictionary().isWord(word.getText().toString().toLowerCase());
+                    this.word.setText("");
+                    for (int i = 0; i < 25; i++) {
+                        lettersTextView[i].setTypeface(null, Typeface.NORMAL);
+                        lettersTextView[i].setBackgroundColor(0xFFFFFF);
+                    }
+                    break;
+            }
+        } catch(IndexOutOfBoundsException e){
+
+        }
+        return false;
     }
 
 
---
+++ app/src/main/java/com/rsmith/wordup/OxfordDictionary.java
@@ -0,0 +1,71 @@
+package com.rsmith.wordup;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by RSmit on 12/9/2017.
+ */
+
+public class OxfordDictionary {
+
+    public boolean isWord(String word) {
+        new CallbackTask().execute(dictionaryEntries(word));
+        return true;
+    }
+
+    private String dictionaryEntries(String word) {
+        final String language = "en";
+        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
+        return "https://od-api.oxforddictionaries.com/api/v1/entries/" + language + "/" + word_id;
+    }
+
+
+    //in android calling network requests on the main thread forbidden by default
+    //create class to do async job
+    private class CallbackTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            //TODO: replace with your own app id and app key
+            final String app_id = "55181793";
+            final String app_key = " f30b80bfa47ae8054375d852e31ee99f";
+            try {
+                URL url = new URL(params[0]);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("Accept","application/json");
+                urlConnection.setRequestProperty("app_id",app_id);
+                urlConnection.setRequestProperty("app_key",app_key);
+
+                // read the output from the server
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+
+                return stringBuilder.toString();
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                return e.toString();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            System.out.println(result);
+        }
+    }
+}
